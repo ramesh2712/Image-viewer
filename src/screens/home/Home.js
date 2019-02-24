bbb17ae443166f3b3f4a4c34148e196fd8a07537
PrernaPardhy <prerna.pardhy@gmailcom>
2019-02-24 20:20:08 +0530

    Home page posts display done

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 import logo from '../../assets/logo.png';
 import search from '../../assets/search.svg';
+import likesnum from '../../assets/likesnum.png';
 import './Home.css';
 import { withStyles } from '@material-ui/core/styles';
-import Input from '@material-ui/core/Input';
-import IconButton from '@material-ui/core/IconButton';
+
+
 import MenuItem from '@material-ui/core/MenuItem';
 import MenuList from '@material-ui/core/MenuList';
 import Divider from '@material-ui/core/Divider';
 
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import Avatar from '@material-ui/core/Avatar';
 
-
+import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
 
 const styles = theme => ({
     button: {
@@ -38,6 +52,32 @@ const styles = theme => ({
     input: {
         display: 'none',
     },
+
+    card: {
+        maxWidth: 400,
+    },
+    formControl: {
+        margin: theme.spacing.unit,
+        minWidth: 20,
+        maxWidth: 100
+    },
+    gridListMain1: {
+        display: "flex",
+        transform: 'translateZ(0)',
+        cursor: 'pointer',
+        border: "1px solid rgba(0,0,0,.3)"
+
+    },
+
+    imgSize: {
+        width: 250,
+        height: 200
+
+    },
+
+    title: {
+        color: theme.palette.primary.light,
+    }
 });
 class Home extends Component {
 
@@ -49,7 +89,9 @@ class Home extends Component {
             usernames: "",
             menuLogo: "dispNone",
             data: [],
-            dataPosts: []
+            dataPosts: [],
+            inputValue: "",
+            gridRequired : "dispBlock"
 
         }
     }
@@ -90,8 +132,10 @@ class Home extends Component {
                 that.setState({
                     dataPosts: data
                 });
-                console.log(that.state.dataPosts)
-                console.log(that.state.dataPosts[1].images.low_resolution.url)
+                // console.log(that.state.dataPosts)
+                //  console.log(that.state.dataPosts[1].images.low_resolution.url)
+                console.log(that.state.dataPosts[1].caption.text)
+                console.log(that.state.dataPosts[1].tags)
             }
 
         });
@@ -100,6 +144,14 @@ class Home extends Component {
         xhrPosts.setRequestHeader("Content-Type", "application/json");
         xhrPosts.withCredentials = false;
         xhrPosts.send(dataPosts);
+
+    }
+
+    searchCaption = (e) => {
+        this.setState({ inputValue: e.target.value });
+        this.setState({gridRequired:'dispNone'});
+
+
 
     }
     render() {
@@ -112,12 +164,11 @@ class Home extends Component {
                     <img src={logo} alt="logo" />
 
                     <span className="white"><img src={search} />
-                        <Input type="text" disableUnderline={true} placeholder="Search.." />
+                        <Input type="text" disableUnderline={true} placeholder="Search.." onChange={this.searchCaption} />
                         <IconButton className={classes.button} onClick={this.onClickProfileIcon}> <img className="profile-img" src={this.state.data.profile_picture} />
                         </IconButton>
 
                     </span>
-
 
                 </header>
                 <span className={this.state.menuLogo}>
@@ -129,8 +180,65 @@ class Home extends Component {
                     </MenuList>
                 </span>
 
+                <div className="gridListMain">
+                    {this.state.inputValue === "" && 
+                    <GridList cellHeight={600} cols={2} className={this.gridRequired} >
+                        {this.state.dataPosts.map(posts => (
+                            <GridListTile key={"grid" + posts.id}>
+                                <Card>
+                                    <CardHeader
+                                        avatar={
+                                            <Avatar aria-label="Recipe" className={classes.avatar}>
+                                                <img src={posts.user.profile_picture} />
+                                            </Avatar>
+                                        }
+                                        action={<img src={posts.profile_picture} />}
+                                        title={posts.user.username}
+                                        subheader={posts.created_time} />
+                                    <CardContent>
+                                        <Typography>
+                                            <img className={classes.imgSize} src={posts.images.low_resolution.url} />
+                                        </Typography>
+                                        <Typography>
+                                            {(posts.caption.text).slice(0, (posts.caption.text).indexOf("#"))}
+                                        </Typography>
+                                        <Typography className={classes.title}>
+                                            {(posts.caption.text).slice((posts.caption.text).indexOf("#"))}
+                                        </Typography>
+
+                                        <Typography>
+                                            {posts.likes.count + " Likes"}
+                                        </Typography>
+                                        <Typography><img src={likesnum} /></Typography>
+                                        <Typography>{posts.comments.count}</Typography>
+                                        <TextField
+                                            id="releaseDateStart"
+                                            label="Add Comment"
+                                            type="text"
+                                            defaultValue=""
+
+                                        />
+                                        <Button variant="contained" color="primary" className={classes.formControl}>
+                                            add
+                                    </Button>
+
+                                    </CardContent>
+                                </Card>
+
+                            </GridListTile>
+                        ))}
+                    </GridList>
+                    }
+
+                </div>
 
             </div>
+
+
+
+
+
+
         )
 
     }
