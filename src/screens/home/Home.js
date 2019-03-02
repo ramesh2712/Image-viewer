636a3b2db56c0630257d8d11992c4a67ec03f19b
Ramesh Prajapati <ramesh.prajapathi@asiglobal.co>
2019-03-02 19:42:44 +0530

    Working on conflict

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../../assets/logo.png';
 import search from '../../assets/search.svg';
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> dfb5fe5c54e9ca9267872e6b5fe7bd6b9d1652fe
 import likesnum from '../../assets/likesnum.png';
 import './Home.css';
 import { withStyles } from '@material-ui/core/styles';
@@ -14,14 +10,6 @@ import MenuItem from '@material-ui/core/MenuItem';
 import MenuList from '@material-ui/core/MenuList';
 import Divider from '@material-ui/core/Divider';
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 99efbdec3238e587c5c4192d17e9a3a724b49521
->>>>>>> dfb5fe5c54e9ca9267872e6b5fe7bd6b9d1652fe
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
@@ -37,65 +25,6 @@ import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
-<<<<<<< HEAD
-=======
-import './Home.css';
-import { withStyles } from '@material-ui/core/styles';
-import Input from '@material-ui/core/Input';
-import IconButton from '@material-ui/core/IconButton';
-import MenuItem from '@material-ui/core/MenuItem';
-import MenuList from '@material-ui/core/MenuList';
-import Divider from '@material-ui/core/Divider';
-
->>>>>>> homepage header done
-
-const styles = theme => ({
-    button: {
-        margin: theme.spacing.unit,
-        position: "fixed",
-        right: 10,
-        top: 0,
-        width: 25
-    },
-
-    dividerLine: {
-        width: 80,
-        align: "center"
-    },
-
-    menuPopup: {
-        background: "#D3D3D3",
-        width: 120,
-        borderRadius: 10,
-        textAlign: "center",
-        position: "absolute",
-        right: 10,
-        borderBottom: "1px solid white"
-    },
-    input: {
-        display: 'none',
-    },
-<<<<<<< HEAD
-
-    card: {
-        maxWidth: 400,
-    },
-    formControl: {
-        margin: theme.spacing.unit,
-        minWidth: 20,
-        maxWidth: 100
-    },
-    gridListMain1: {
-        display: "flex",
-        transform: 'translateZ(0)',
-        cursor: 'pointer',
-        border: "1px solid rgba(0,0,0,.3)"
-
-=======
-<<<<<<< HEAD
->>>>>>> Home page posts display done
-=======
->>>>>>> 99efbdec3238e587c5c4192d17e9a3a724b49521
 
 const styles = theme => ({
     button: {
@@ -138,7 +67,6 @@ const styles = theme => ({
         cursor: 'pointer',
         border: "1px solid rgba(0,0,0,.3)"
 
->>>>>>> dfb5fe5c54e9ca9267872e6b5fe7bd6b9d1652fe
     },
 
     imgSize: {
@@ -150,11 +78,6 @@ const styles = theme => ({
     title: {
         color: theme.palette.primary.light,
     }
-<<<<<<< HEAD
-=======
->>>>>>> homepage header done
-=======
->>>>>>> dfb5fe5c54e9ca9267872e6b5fe7bd6b9d1652fe
 });
 class Home extends Component {
 
@@ -166,19 +89,9 @@ class Home extends Component {
             usernames: "",
             menuLogo: "dispNone",
             data: [],
-<<<<<<< HEAD
-<<<<<<< HEAD
             dataPosts: [],
             inputValue: "",
             gridRequired : "dispBlock"
-=======
-            dataPosts: []
->>>>>>> homepage header done
-=======
-            dataPosts: [],
-            inputValue: "",
-            gridRequired : "dispBlock"
->>>>>>> dfb5fe5c54e9ca9267872e6b5fe7bd6b9d1652fe
 
         }
     }
@@ -192,13 +105,6 @@ class Home extends Component {
         let xhr = new XMLHttpRequest();
         let that = this;
         xhr.addEventListener("readystatechange", function () {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-
-=======
->>>>>>> 99efbdec3238e587c5c4192d17e9a3a724b49521
->>>>>>> dfb5fe5c54e9ca9267872e6b5fe7bd6b9d1652fe
             if (this.readyState === 4) {
 
                 that.setState({
@@ -212,10 +118,7 @@ class Home extends Component {
         xhr.setRequestHeader("Content-Type", "application/json");
         xhr.withCredentials = false;
         xhr.send(data);
-<<<<<<< HEAD
 
-=======
->>>>>>> 99efbdec3238e587c5c4192d17e9a3a724b49521
         let dataPosts = null;
         let xhrPosts = new XMLHttpRequest();
 
@@ -250,54 +153,6 @@ class Home extends Component {
         this.setState({gridRequired:'dispNone'});
 
 
-
-        let dataPosts = null;
-        let xhrPosts = new XMLHttpRequest();
-
-
-        xhrPosts.addEventListener("readystatechange", function () {
-            if (this.readyState === 4) {
-                var myObject = JSON.parse(this.responseText);
-                // console.log(myObject);
-                var data = myObject.data;
-                //console.log(data);
-
-                that.setState({
-                    dataPosts: data
-                });
-<<<<<<< HEAD
-                // console.log(that.state.dataPosts)
-                //  console.log(that.state.dataPosts[1].images.low_resolution.url)
-                console.log(that.state.dataPosts[1].caption.text)
-                console.log(that.state.dataPosts[1].tags)
-=======
-                console.log(that.state.dataPosts)
-                console.log(that.state.dataPosts[1].images.low_resolution.url)
->>>>>>> homepage header done
-            }
-
-        });
-        xhrPosts.open("GET", this.props.baseUrl + "users/self/media/recent?access_token=8661035776.d0fcd39.87fd934e04f84253aaf234d8bd4e4c65");
-        xhrPosts.setRequestHeader("Cache-Control", "no-cache");
-        xhrPosts.setRequestHeader("Content-Type", "application/json");
-        xhrPosts.withCredentials = false;
-        xhrPosts.send(dataPosts);
-
-    }
-<<<<<<< HEAD
-=======
-    render() {
-        const { classes } = this.props;
-<<<<<<< HEAD
->>>>>>> homepage header done
-
-    searchCaption = (e) => {
-        this.setState({ inputValue: e.target.value });
-        this.setState({gridRequired:'dispNone'});
-
-
-=======
->>>>>>> dfb5fe5c54e9ca9267872e6b5fe7bd6b9d1652fe
 
     }
     render() {
@@ -307,8 +162,6 @@ class Home extends Component {
             <div className="app-header">
                 <header className="app-header">
                     <img src={logo} alt="logo" />
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 
                     <span className="white"><img src={search} />
@@ -318,33 +171,6 @@ class Home extends Component {
 
                     </span>
 
-=======
-                    <span className="white"><img src={search} />
-                        <Input type="text" disableUnderline={true} placeholder="Search.." />
-                        <IconButton className={classes.button} onClick={this.onClickProfileIcon}> <img className="profile-img" src={this.state.data.profile_picture} />
-                        </IconButton>
-                    </span>
->>>>>>> homepage header done
-=======
-=======
-
->>>>>>> 99efbdec3238e587c5c4192d17e9a3a724b49521
-                    <span className="white"><img src={search} />
-                        <Input type="text" disableUnderline={true} placeholder="Search.." onChange={this.searchCaption} />
-                        <IconButton className={classes.button} onClick={this.onClickProfileIcon}> <img className="profile-img" src={this.state.data.profile_picture} />
-                        </IconButton>
-<<<<<<< HEAD
-                    </span>
-<<<<<<< HEAD
-=======
-
->>>>>>> Home page posts display done
-=======
-
-                    </span>
-
->>>>>>> 99efbdec3238e587c5c4192d17e9a3a724b49521
->>>>>>> dfb5fe5c54e9ca9267872e6b5fe7bd6b9d1652fe
                 </header>
                 <span className={this.state.menuLogo}>
                     <MenuList className={classes.menuPopup} disableUnderline="true">
@@ -354,14 +180,6 @@ class Home extends Component {
                         <MenuItem>Logout</MenuItem>
                     </MenuList>
                 </span>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 99efbdec3238e587c5c4192d17e9a3a724b49521
->>>>>>> dfb5fe5c54e9ca9267872e6b5fe7bd6b9d1652fe
 
                 <div className="gridListMain">
                     {this.state.inputValue === "" && 
@@ -388,7 +206,6 @@ class Home extends Component {
                                         <Typography className={classes.title}>
                                             {(posts.caption.text).slice((posts.caption.text).indexOf("#"))}
                                         </Typography>
-<<<<<<< HEAD
 
                                         <Typography>
                                             {posts.likes.count + " Likes"}
@@ -401,20 +218,6 @@ class Home extends Component {
                                             type="text"
                                             defaultValue=""
 
-=======
-
-                                        <Typography>
-                                            {posts.likes.count + " Likes"}
-                                        </Typography>
-                                        <Typography><img src={likesnum} /></Typography>
-                                        <Typography>{posts.comments.count}</Typography>
-                                        <TextField
-                                            id="releaseDateStart"
-                                            label="Add Comment"
-                                            type="text"
-                                            defaultValue=""
-
->>>>>>> dfb5fe5c54e9ca9267872e6b5fe7bd6b9d1652fe
                                         />
                                         <Button variant="contained" color="primary" className={classes.formControl}>
                                             add
@@ -430,15 +233,6 @@ class Home extends Component {
 
                 </div>
 
-<<<<<<< HEAD
-=======
->>>>>>> homepage header done
-=======
-<<<<<<< HEAD
->>>>>>> Home page posts display done
-=======
->>>>>>> 99efbdec3238e587c5c4192d17e9a3a724b49521
->>>>>>> dfb5fe5c54e9ca9267872e6b5fe7bd6b9d1652fe
             </div>
 
 
@@ -450,14 +244,5 @@ class Home extends Component {
 
     }
 }
-<<<<<<< HEAD
 
 export default withStyles(styles)(Home);
-=======
-<<<<<<< HEAD
-
-export default withStyles(styles)(Home);
-=======
-export default withStyles(styles)(Home);
->>>>>>> 99efbdec3238e587c5c4192d17e9a3a724b49521
->>>>>>> dfb5fe5c54e9ca9267872e6b5fe7bd6b9d1652fe
