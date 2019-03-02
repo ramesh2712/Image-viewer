244c833dddd397f630573f3fa16c8b631c049cc6
Ramesh Prajapati <ramesh.prajapathi@asiglobal.co>
2019-03-02 18:25:18 +0530

    Removed unnecessary import statement

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import './Login.css';
-import Home from '../home/Home'
-
-import logo from '../../assets/logo.png';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import FormControl from '@material-ui/core/FormControl';
@@ -12,7 +8,6 @@ import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
 import FormHelperText from '@material-ui/core/FormHelperText';
-import { Link } from 'react-router-dom';
 import Header from '../../common/header/Header';
 
 class Login extends Component {
